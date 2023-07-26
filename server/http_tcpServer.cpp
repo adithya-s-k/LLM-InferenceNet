4041fd9ce76b6c033e0067bcfd7af98962c906f0
kanuar <kodwaniraunak1@gmail.com>
2023-07-26 15:31:42 +0530

    updates on the files

--- server/http_tcpServer.cpp
+++ server/http_tcpServer.cpp
@@ -1,13 +1,80 @@
 #include <http_tcpServer.h>
+#include <iostream>
+#include <sstream>
+#include <unistd.h>
+
+
+namespace
+{
+	void log(const std::string& user_message)
+	{
+		std::cout<<user_message<<endl;
+	}
+
+	void ErrorExitCode(const std::string& server_error_message)
+	{
+		log("Error: "+server_error_message);
+		exit(1);
+	}
+}
+
 namespace http
 {
-	TcpServer::TcpServer()
-	{
 
+	TcpServer::TcpServer(std::string ip,int portno): user_ip(ip),user_portno(portno),user_socketid(),user_newsocket(),user_incomingmsg(),lenght_user_socketAddress(sizeof(user_socketAddress)),user_serverMessage(buildResponse())
+	{
+		startServer();
 	}
+
+	int TcpServer::startServer()
+	{
+		user_socketid=socket(AF_INET, SOCK_STREAM,0);
+		if (user_socketid<0)
+		{
+			ErrorExitCode("Socket could not be created");
+			return 1;
+		}
+		return 0;
+	}
+	void TcpServer::closeServer()
+	{
+		close(user_socketid);
+		close(user_newsocket);
+		exit(0);
+	}
+	
+	void TcpServer::startListen()
+	{
+	    if (listen(user_socketid, 20) < 0)
+	    {
+	        exitWithError("Socket listen failed");
+	    }
+	    std::ostringstream ss;
+	    ss << "\n*** Listening on ADDRESS: " 
+	        << inet_ntoa(user_socketAddress.sin_addr) 
+	        << " PORT: " << ntohs(user_socketAddress.sin_port) 
+	        << " ***\n\n";
+	    log(ss.str());
+	}
+
+	void TcpServer::acceptConnection(SOCKET &new_socket)
+	{
+	    new_socket = accept(user_socketid, (input_adder *)&user_socketAddress, 
+	                        &lenght_user_socketAddress);
+	    if (new_socket < 0)
+	    {
+	        std::ostringstream ss;
+	        ss << 
+	        "Server failed to accept incoming connection from ADDRESS: " 
+	        << inet_ntoa(user_socketAddress.sin_addr) << "; PORT: " 
+	        << ntohs(user_socketAddress.sin_port);
+	        exitWithError(ss.str());
+	    }
+	}
+
 	TcpServer::~TcpServer()
 	{
-
+		closeServer();
 	}
-} //namespace http
 
+} //namespace http
--- server/server.cpp
+++ server/server.cpp
@@ -1,6 +1,6 @@
 int main(int argc, char const *argv[])
 {
 	using namespace http;
-	TcpServer server= TcpServer();
+	TcpServer server= TcpServer("0.0.0.0",8080);
 	return 0;
 }
